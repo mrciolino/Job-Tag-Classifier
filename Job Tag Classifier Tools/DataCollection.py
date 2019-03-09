f8742bd99c9d4eb8cda748450b577ec820e1ebf4
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-03-09 09:56:42 -0600

    convert data ingest to sql

--- Job Tag Classifier Tools/DataCollection.py
+++ Job Tag Classifier Tools/DataCollection.py
@@ -4,14 +4,21 @@ Collection of data collection functions that imports
 and cleans our data before feature engineering
 """
 import pandas as pd
+import psycopg2
 import sys
 
+sql_connection_string = "dbname='Cutback' host='127.0.0.1'"
 
-def load_data(data_file):
+
+def load_data(sql_connection_string):
     try:
-        df = pd.read_csv(data_file)
+        conn = psycopg2.connect(sql_connection_string)
+        sql = "select * from job_data;"
+        data = pd.io.sql.read_sql_query(sql, conn)
+        pd.io.sql.
+        conn = None
     except:
-        print("Unable to read data into pandas datafram")
+        print("Unable to read sql database into pandas datafram")
         sys.exit(0)
     return df
 
@@ -23,3 +30,11 @@ def remove_empty_rows(df):
     except:
         print("Unable to remove empty row from the dataframe")
         sys.exit(0)
+
+
+def data_collection(sql_connection_string):
+
+    df = load_data(sql_connection_string)
+    df = remove_empty_rows(df)
+
+    return df
