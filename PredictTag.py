e60a50b918355cd4722b6333fc57fb429fbed9ee
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-03-15 09:28:45 -0500

    added functions for updating classifiers

--- PredictTag.py
+++ PredictTag.py
@@ -2,30 +2,32 @@
 Cutback.io
 Our pipeline for tagging new jobs and optimizing the model as more data comes in
 """
+
 from keras import models
 import pandas as pd
 import sys
 
 sys.path.append("Job Tag Classifier Tools")
-from Pipeline import DataLoader, tag_decoder
+from Pipeline import *
 
 # load models
-model_file =
-model = models.load_model(model_file)
+model_file = "Models"
+# model = models.load_model(model_file)
 
 # read in new job to classifiy ----- Change to select new from job_data
-sql_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_data;"]
-X, _, Y, _ = DataLoader(sql_string, test_size=0)
+sql_import_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_data LIMIT 1 OFFSET 20;"]
+X, _, Y, _ = DataLoader(sql_import_string, test_size=0)
 
 # predict tags
-list_of_indices = model.predict(X)
+#list_of_indices = model.predict(X)
 
-# decode the target back into text
-predicition = tag_decoder(list_of_indices, threshold = .2)
+# decode the target back into text - make sur excepts may rows of data
+#predicition = tag_decoder(list_of_indices, threshold=.2)
 
+# add the new data to a sql table to save for a training batch
+sql_add_new_data_string = ["dbname='Cutback' host='127.0.0.1'", "new_data"]
+update = batch_new_data(sql_import_string, sql_add_new_data_string)
 
-
-# add new data to batch to be trained when it is full
-# use our predicited tags as the target and hope accuracy is good enough
-# fill batcha and re train models
-# empty batch and restart process
+# update the model and reset the new_data table
+if update:
+    update_classifier()
