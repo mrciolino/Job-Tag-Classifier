d0853a5b7f76567cad9b512bb30a767318be42e8
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-03-16 13:41:04 -0500

    corrected how we add new data and re-enabled predict functions

--- PredictTag.py
+++ PredictTag.py
@@ -19,13 +19,13 @@ sql_import_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_dat
 X, _, Y, _ = DataLoader(sql_import_string, test_size=0)
 
 # predict tags
-#list_of_indices = model.predict(X)
+list_of_indices = model.predict(X)
 
 # decode the target back into text - make sur excepts may rows of data
-#predicition = tag_decoder(list_of_indices, threshold=.2)
+predicition = tag_decoder(list_of_indices, threshold=.2)
 
 # add the new data to a sql table to save for a training batch
-sql_add_new_data_string = ["dbname='Cutback' host='127.0.0.1'", "new_data"]
+sql_add_new_data_string = ['postgresql://127.0.0.1/Cutback', "new_data"]
 update = batch_new_data(sql_import_string, sql_add_new_data_string)
 
 # update the model and reset the new_data table
