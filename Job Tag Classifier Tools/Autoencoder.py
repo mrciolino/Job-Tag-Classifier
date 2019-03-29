5426e1fbd2d8d2235f02ee1fd8d7377c972f11c6
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-03-28 21:23:10 -0500

    update network with more dense layers and added network names for both the AE and the encoder

--- Job Tag Classifier Tools/Autoencoder.py
+++ Job Tag Classifier Tools/Autoencoder.py
@@ -1,6 +1,6 @@
-from keras.callbacks import TensorBoard
 from keras.models import Model
 from keras import layers
+import numpy as np
 
 import sys
 sys.path.append("Job Tag Classifier Tools")
@@ -9,22 +9,31 @@ from Pipeline import DataLoader
 sql_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_data;"]
 X_train, X_test, _, _ = DataLoader(sql_string, test_size=.99)
 
-# length of input/output
-num_varibles = X_train.shape[1]
+# reshape data to fit into our model
+num_varibles = X_test.shape[1]
+X_test = X_test[:, :, np.newaxis]
+X_test.shape
+
 
 def model(num_varibles):
 
-    input = layers.Input(shape=(num_varibles,None,None))
+    input = layers.Input(shape=(num_varibles, 1))
     encoder_conv_1 = layers.Conv1D(250, 10, padding='same')(input)
     enconder_pool_1 = layers.MaxPooling1D(2, padding='same')(encoder_conv_1)
     encoder_conv_2 = layers.Conv1D(200, 8, padding='same')(enconder_pool_1)
-    enconder_pool_1 = layers.MaxPooling1D(2, padding='same')(encoder_conv_2)
-    encoder_conv_3 = layers.Conv1D(150, 6, padding='same')(enconder_pool_1)
-    encoded = layers.MaxPooling1D(2, padding='same')(encoder_conv_3)
+    enconder_pool_2 = layers.MaxPooling1D(2, padding='same')(encoder_conv_2)
+    encoder_conv_3 = layers.Conv1D(150, 6, padding='same')(enconder_pool_2)
+    enconder_pool_3 = layers.MaxPooling1D(2, padding='same')(encoder_conv_3)
 
-    dense = layers.Dense(1500)(encoded)
-    dense_encoded = layers.Dense(500)(dense)
-    dense = layers.Dense(1500)(dense_encoded)
+    dense = layers.Dense(500)(enconder_pool_3)
+    dense = layers.Dense(400)(dense)
+    dense = layers.Dense(300)(dense)
+
+    encoded = layers.Dense(100)(dense)
+
+    dense = layers.Dense(300)(encoded)
+    dense = layers.Dense(400)(dense)
+    dense = layers.Dense(500)(dense)
 
     decoder_conv_1 = layers.Conv1D(150, 6, padding='same')(dense)
     decoder_upsample_1 = layers.UpSampling1D(2)(decoder_conv_1)
@@ -34,13 +43,17 @@ def model(num_varibles):
     decoder_upsample_3 = layers.UpSampling1D(2)(decoder_conv_3)
     decoded = layers.Conv1D(1, 0, padding='same')(decoder_upsample_3)
 
-    autoencoder = Model(input, decoded)
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    autoencoder = Model(inputs=input, outputs=decoded, name='AE')
+    encoder = Model(inputs=input, outputs=encoded, name='encoder')
 
-    return autoencoder
+    autoencoder.compile(optimizer='adam', loss='mse')
+    encoder.compile(optimizer='adam', loss='mse')
 
-num_varibles
-autoencoder = model(num_varibles)
+    return autoencoder, encoder
+
+
+autoencoder, encoder = model(num_varibles)
 autoencoder.summary()
+encoder.summary()
 
-autoencoder.fit(X_test, X_test, epochs=2, batch_size=10)
+autoencoder.fit(X_test, X_test, epochs=2, batch_size=10, verbose=1)
