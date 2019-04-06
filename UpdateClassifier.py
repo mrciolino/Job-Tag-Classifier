cc359267a9f675aa52bf4f12e48d9235c26c1b78
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-04-06 10:41:13 -0500

    added the encoder step to the new data pipeline

--- UpdateClassifier.py
+++ UpdateClassifier.py
@@ -1,5 +1,5 @@
 """
-Matthew Ciolino - Job Tag Classifier 
+Matthew Ciolino - Job Tag Classifier
 Update the classifier when we have enough new data to process
 """
 
@@ -9,19 +9,19 @@ import sys
 sys.path.append("Job Tag Classifier Tools")
 from Pipeline import DataLoader
 
-# load model weights
-model_file = "Models"
-model = models.load_model(model_file)
-
 # load new_data sql table
+# ["dbname='Cutback' host='localhost' port='5432' user='postgres' password='1234'", "select * from job_data;"]
+# ["dbname='Cutback' host='127.0.0.1'", "select * from new_data;"]
 sql_import_string = ["dbname='Cutback' host='127.0.0.1'", "select * from new_data"]
 X, _, Y, _ = DataLoader(sql_import_string, test_size=0)
 
-# train the model
-model.fit(X_train, Y_train, epochs=10, batch_size=25)
+# encode the Input
+encoder = models.load_model("Models/encoder_model.hd5")
+X_encoded = encoder.predict(X)
 
-# evaluate the model
-model.evaluate(X_test, Y_test, verbose=0)
+# train the model
+model = models.load_model("Models/classifier_model.hd5"")
+model.fit(X_encoded, Y, epochs=5, batch_size=5)
 
 # update weights
 model.save("Models/model.hd5")
