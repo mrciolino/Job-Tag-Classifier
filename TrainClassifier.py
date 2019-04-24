974a38e6f5fc0834c5e650aa70c9aa15c6edb06b
Matt <mrciolino@comcast.net>
2019-04-24 08:19:50 -0500

    adding the encoder to the pipeline

--- TrainClassifier.py
+++ TrainClassifier.py
@@ -3,7 +3,7 @@ Matthew Ciolino - Job Tag Classifier
 Our AI model that is used to predict job tags
 """
 from keras.callbacks import TensorBoard
-from keras.models import Sequential
+from keras.models import Model
 from keras import layers, models
 from time import time
 import sys
@@ -13,32 +13,34 @@ from Pipeline import DataLoader
 
 # ["dbname='Cutback' host='localhost' port='5432' user='postgres' password='1234'", "select * from job_data;"]
 # ["dbname='Cutback' host='127.0.0.1'", "select * from job_data;"]
-sql_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_data;"]
+sql_string = ["dbname='Cutback' host='localhost' port='5432' user='postgres' password='1234'", "select * from job_data;"]
 X_train, X_test, Y_train, Y_test = DataLoader(sql_string, test_size=.2)
 
 # open channel for TensorBoard
-tensorboard = TensorBoard(log_dir="Logs/{}".format(time()),
+tensorboard = TensorBoard(log_dir="Logs/classifier/{}".format(time()),
                           histogram_freq=1,
                           write_grads=True)
 
 
 def classification_model(num_varibles, num_classes):
     # create model
-    model = Sequential()
-    model.add(layers.Input(shape=(num_varibles,)))
-    model.add(layers.Dense(200, activation='relu'))
-    model.add(layers.Dropout(.2))
-    model.add(layers.Dense(150, activation='relu'))
-    model.add(layers.Dropout(.2))
-    model.add(layers.Dense(100, activation='relu'))
-    model.add(layers.Dropout(.2))
-    model.add(layers.Dense(50, activation='relu'))
-    model.add(layers.Dropout(.2))
-    model.add(layers.Dense(25, activation='relu'))
-    model.add(layers.Dropout(.2))
-    model.add(layers.Dense(num_classes, activation='sigmoid'))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    return model
+    input = layers.Input(shape=(num_varibles, ))
+    dense = layers.Dense(200, activation='relu')(input)
+    dense = layers.Dropout(.2)(dense)
+    dense = layers.Dense(150, activation='relu')(dense)
+    dense = layers.Dropout(.2)(dense)
+    dense = layers.Dense(100, activation='relu')(dense)
+    dense = layers.Dropout(.2)(dense)
+    dense = layers.Dense(50, activation='relu')(dense)
+    dense = layers.Dropout(.2)(dense)
+    dense = layers.Dense(25, activation='relu')(dense)
+    dense = layers.Dropout(.2)(dense)
+    output = layers.Dense(num_classes, activation='sigmoid')(dense)
+
+    classifier = Model(inputs=input, outputs=output, name='Classifier')
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return classifier
 
 
 # encode the Input
@@ -52,10 +54,10 @@ num_classes = Y_train.shape[1]
 model = classification_model(num_varibles, num_classes)
 
 # fit the model
-model.fit(X_train_encoded, Y_train, validation_data=(X_test_encoded, Y_test), epochs=1, batch_size=1, callbacks=[tensorboard])
+model.fit(X_train_encoded, Y_train, validation_data=(X_test_encoded, Y_test), epochs=100, batch_size=10, callbacks=[tensorboard])
 
 # evaluate the model
-model.evaluate(X_test, Y_test, verbose=1)
+model.evaluate(X_test_encoded, Y_test, verbose=1)
 
 # save the model
-model.save("Models/classifier_model.hd5")
+model.save("Models/classifier_model")
