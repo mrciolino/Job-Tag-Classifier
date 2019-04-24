7fdc4b842c350f09cf7596656d88926ca300f674
Matthew Ciolino <mrciolino@Matthews-MacBook-Pro.local>
2019-04-23 19:01:59 -0500

    remove rows in dataframe that contain a non-english words - there were a few russian jobs that our fixed sized feature hasher would blow up in size

--- Job Tag Classifier Tools/DataCollection.py
+++ Job Tag Classifier Tools/DataCollection.py
@@ -1,9 +1,10 @@
 """
-Matthew Ciolino - Job Tag Classifier 
+Matthew Ciolino - Job Tag Classifier
 Collection of data collection functions that imports
 and cleans our data before feature engineering
 """
 from sqlalchemy import create_engine
+from nltk.corpus import wordnet
 import pandas as pd
 import numpy as np
 import traceback
@@ -25,14 +26,26 @@ def load_data(sql_connection_string):
     return df
 
 
-def remove_empty_rows(df):
+def remove_unwanted_rows(df):
+
+    def detect_non_english(sentence, m=0):
+        for words in sentence:
+            if m == 2:
+                return True
+            if not wordnet.synsets(words):
+                return False
+            m = m + 1
+
     try:
+        # remove empty title/descriptions
         df['job_description'].replace('', np.nan, inplace=True)
         df['job_title'].replace('', np.nan, inplace=True)
         df.dropna(subset=['job_description'], inplace=True)
         df.dropna(subset=['job_title'], inplace=True)
+        # remove non english rows
+        df = df[df.job_title.map(lambda x: detect_non_english(x))]
     except:
-        print("ERROR: Unable to remove empty rows from the dataframe")
+        print("ERROR: Unable to remove unwanted rows from the dataframe")
         traceback.print_exc(file=sys.stdout)
         sys.exit(0)
     return df
@@ -43,7 +56,7 @@ def add_new_data(df, sql_string):
         connection = sql_string[0]
         table = sql_string[1]
         engine = create_engine(connection)
-        df.to_sql(name = table, con=engine, if_exists='append', index=False)
+        df.to_sql(name=table, con=engine, if_exists='append', index=False)
     except:
         print("ERROR: Unable to save new dataframe to sql table")
         traceback.print_exc(file=sys.stdout)
@@ -51,7 +64,8 @@ def add_new_data(df, sql_string):
 
     string = "select count(*) from " + table
     result = engine.execute(string)
-    for row in result: length = row[0]
+    for row in result:
+        length = row[0]
     engine.dispose()
 
     return True if length > 25 else False
@@ -60,6 +74,6 @@ def add_new_data(df, sql_string):
 def data_collection(sql_connection_string):
 
     df = load_data(sql_connection_string)
-    df = remove_empty_rows(df)
+    df = remove_unwanted_rows(df)
 
     return df
