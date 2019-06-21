53d594f484329e75b3f06ed78644da810e772ec7
Matthew Ciolino <mrciolino@comcast.net>
2019-06-20 19:19:06 -0500

    database name update

--- PredictTag.py
+++ PredictTag.py
@@ -10,9 +10,9 @@ sys.path.append("Job Tag Classifier Tools")
 from Pipeline import DataLoader, BatchData
 import UpdateClassifier
 
-# ["dbname='Cutback' host='localhost' port='5432' user='postgres' password='1234'", "select * from job_data;"]
-# ["dbname='Cutback' host='127.0.0.1'", "select * from job_data;"]
-sql_import_string = ["dbname='Cutback' host='127.0.0.1'", "select * from job_data LIMIT 1 OFFSET 20;"]
+# ["dbname='job_tag_classifier' host='localhost' port='5432' user='postgres' password='1234'", "select * from job_data;"]
+# ["dbname='job_tag_classifier' host='127.0.0.1'", "select * from job_data;"]
+sql_import_string = ["dbname='job_tag_classifier' host='127.0.0.1'", "select * from job_data LIMIT 1 OFFSET 20;"]
 X, _, Y, _ = DataLoader(sql_import_string, test_size=0)
 
 
@@ -28,7 +28,7 @@ list_of_indices = model.predict(X_encoded)
 predicition = tag_decoder(list_of_indices, threshold=.2)
 
 # add the new data to a sql table to save for a training batch
-sql_add_new_data_string = ['postgresql://127.0.0.1/Cutback', "new_data"]
+sql_add_new_data_string = ['postgresql://127.0.0.1/job_tag_classifier', "new_data"]
 update = BatchData(sql_import_string, sql_add_new_data_string)
 
 # update the model and reset the new_data table
